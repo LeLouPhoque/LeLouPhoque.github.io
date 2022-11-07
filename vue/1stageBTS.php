c2dbcf69f48f67fa820d64f546bad4359caa718c
Esteban <90008993+LeLouPhoque@users.noreply.github.com>
2022-11-07 22:53:42 +0100

    Add files via upload

---
+++ vue/1stageBTS.php
@@ -0,0 +1,60 @@
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="../style/style.css" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<title>
+			Portfolio Esteban Rautureau
+		</title>
+	</head>
+	<body>
+		<nav>
+			<div class="profile">
+				<img src="https://lh3.googleusercontent.com/a-/ACNPEu84SrPJx1SQ94gvg-9YrtmAVjpBzh-kpfRihXM0zQ=s83-c-mo" alt="profile photo" />
+				<h1>Esteban Rautureau</h1>
+			</div>
+			<ul>
+            <li>
+                
+                <a href="../index.html"  >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                        <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+                    </svg>
+                </a>
+                <li><div class="dropdown">
+                    <button>Outils dispo <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6z"/>
+                    </svg></button>
+                <div class="dropdown-content">
+                <a href="https://larrybolt.github.io/online-ics-feed-viewer/" target="_blank">ics viewer</a>
+                <a href="https://dev.mailjet.com/email/guides/getting-started/#view-message-history" target="_blank">mailjet</a>
+                <a href="../diapo/C__wamp64_www_LeLouPhoque.github.io_diapo_Main.pdf" target="_blank">mailjet code</a>
+                <a href="https://www.codingame.com/profile/7916a88be023a3bc2364577e633dc4755284234">Coding game</a>
+                </div></li>
+                </li>
+				<li><a href="https://github.com/LeLouPhoque" target="_blank" >Github</a></li>
+				<li><a href="https://www.linkedin.com/in/esteban-rautureau/" target="_blank" >Linkedin</a></li>
+				<li><a href="#">Me contacter</a></li>
+			</ul>
+		</nav>
+        <section class="page-content">
+			<header>
+				<div class="shine">
+				<h3>Stage BTS 2022<br/><br/></h3>
+                </div>
+                <embed src="../diapo/ORAL STAGE.pdf" width=1200 height=1000 type='application/pdf'/>
+                <br /><br />
+            </header>
+        
+        <img src="../image/img5.png"></img>
+        <footer >
+			<div>
+				<p>ESTEBAN RAUTUREAU &copy; 2022</p>
+			</div>
+		</footer>
+</section>
+</body>
